2efe401005e7d2de1f1fdf663a2d36603c3764ba
Anurag <anusrivas07@gmail.com>
2023-05-04 12:59:13 +0530

    "dbhelper_registrationform"

---
+++ android/app/src/main/kotlin/com/example/dbhelper_registrationform/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dbhelper_registrationform
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/InputAndOutputQuestions.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Input extends StatefulWidget {
+  const Input({Key? key}) : super(key: key);
+
+  @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Demo Test0'),
+      ),
+
+      body: Container(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Input first number',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Input the Second number'
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+
+              }, child: Text('Sum of two numbers'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
