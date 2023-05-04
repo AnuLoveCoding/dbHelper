28295b58dba2f4da5ecaf67c3e9d6c2baef77047
Anurag <anusrivas07@gmail.com>
2023-05-04 19:54:27 +0530

    "dbhelper_registrationform"

--- lib/InputAndOutputQuestions.dart
+++ lib/InputAndOutputQuestions.dart
@@ -38,24 +38,14 @@ class _InputState extends State<Input> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            TextField(
-              controller: text1,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))
-                ),
-                hintText: 'Input first number',
-              ),
-            ),
+            textarea(control: text1, name: 'Input First Number'),
             SizedBox(height: 10.0,),
             textarea(control: text2, name: 'Input Second Number'),
             SizedBox(height: 20.0,),
             ElevatedButton(onPressed: (){
-
               setState(() {
                c = int.parse(text1.text) + int.parse(text2.text);
               });
-
               }, child: Text('Sum of two numbers'),
             ),
             SizedBox(height: 10.0,),
